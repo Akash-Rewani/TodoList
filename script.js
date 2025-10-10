b528f454bdad7ffa99c07af0c0650aa44582ad78
akash <akashrewani8@gmail.com>
2025-10-10 17:07:17 +0530

    add check function

--- script.js
+++ script.js
@@ -4,15 +4,33 @@ const output = document.querySelector('.output');
 
 const todos = [];
 
-function addTask() {
-    const title = input.value
-    todos.push(title);
-    output.innerHTML += `<div>${title}</div>
-    <button>Delete</button>`
-    console.log(todos);
-    input.value = ''
+function check(value, todos) {
+    if (value === '') {
+        input.value = ''
+        return false
+    }
+
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i] === value) {
+            alert("same task")
+            input.value = ''
+            return false
+        }
+    }
+    return true
+
 }
 
+function addTask() {
+    const title = input.value.trim()
+    if (check(title, todos)) {
+        todos.push(title);
+        output.innerHTML += `<div>${title}</div>
+        <button>Delete</button>`
+        console.log(todos);
+        input.value = ''
+    }
+}
 button.addEventListener("click", addTask);
 
 
